359354288f70766e35d66bfb42e3f23082c44e14
Jakkarin <invaderzgame@gmail.com>
2020-07-23 19:04:52 +0700

    build v.0.0.1

---
+++ client/build/precache-manifest.d440c51fd6f1efea647937e91e97718a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed378998ea6b0a9a394c1cb3aefa72ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2bc5ec95c0aea34a39b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "668d127e47d737db6604",
+    "url": "/static/css/main.2d2c89d6.chunk.css"
+  },
+  {
+    "revision": "e2bc5ec95c0aea34a39b",
+    "url": "/static/js/2.8bf21f10.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.8bf21f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668d127e47d737db6604",
+    "url": "/static/js/main.370cbe56.chunk.js"
+  },
+  {
+    "revision": "5a035aeb6ea475b0675e",
+    "url": "/static/js/runtime-main.1b369307.js"
+  },
+  {
+    "revision": "6a8ee48e051f10afe64659f023b1f9ec",
+    "url": "/static/media/epic-auto-claim.6a8ee48e.png"
+  }
+]);
